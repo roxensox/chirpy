5673f5015c41c0e9768afd345f9be1a6b6dbe778
roxensox <ryanhoveskeland@gmail.com>
2025-10-03 12:36:35 -0500

    Added query to update user email and password

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -88,3 +88,37 @@ func (q *Queries) ResetUsers(ctx context.Context) error {
 	_, err := q.db.ExecContext(ctx, resetUsers)
 	return err
 }
+
+const updateUser = `-- name: UpdateUser :one
+UPDATE users
+SET 
+	email = $1,
+	hashed_password = $2
+WHERE id = $3
+RETURNING id, email, created_at, updated_at
+`
+
+type UpdateUserParams struct {
+	Email          string
+	HashedPassword string
+	ID             uuid.UUID
+}
+
+type UpdateUserRow struct {
+	ID        uuid.UUID
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (q *Queries) UpdateUser(ctx context.Context, arg UpdateUserParams) (UpdateUserRow, error) {
+	row := q.db.QueryRowContext(ctx, updateUser, arg.Email, arg.HashedPassword, arg.ID)
+	var i UpdateUserRow
+	err := row.Scan(
+		&i.ID,
+		&i.Email,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+	)
+	return i, err
+}
